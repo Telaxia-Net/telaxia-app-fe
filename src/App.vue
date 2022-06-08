14957c33a2e86ee98d20120806d0052d6289df45
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2022-06-08 04:53:32 -0500

    feat: wow

--- src/App.vue
+++ src/App.vue
@@ -58,8 +58,10 @@ export default {
 * {
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
+  box-sizing: border-box; 
 }
+
+/* This is some text */
 
 html {
   background-color: rgb(255, 242, 218);
